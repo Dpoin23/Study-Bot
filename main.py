fa94d1ee8a64509d1ab9a3974a78c583d112af6f
David Poindexter <davidpoindexter03@gmail.com>
2025-10-09 23:12:58 -0700

    3 events: on ready, on member join, on message, and 1 command: hello, implemented

--- main.py
+++ main.py
@@ -6,3 +6,35 @@ import os
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
+
+handler = logging.FileHandler(filename='discord.og', encoding='utf-8', mode='w')
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='#', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Ready to go: {bot.user.name}')
+
+@bot.event
+async def on_member_join(member):
+    await member.send(f"Welcome to the server {member.name}")
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return 
+    
+    if "shit" in message.content.lower():
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} don't use that word")
+
+    await bot.process_commands(message)
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Hello {ctx.author.mention}!")
+
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
